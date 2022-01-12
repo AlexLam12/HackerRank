81597ab36c155b0692f6f469cdbf23b45cea2853
AlexLam12 <Alex.e015@gmail.com>
2022-01-12 10:02:17 -0600

    test

---
+++ HR1.js
@@ -0,0 +1,48 @@
+function processData(input) {
+    //Enter your code here
+    const titleCase = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+    function processData(input) {
+      const inputArray = (input.includes("\r")) ? input.split("\r\n") : input.split("\n");
+      inputArray.forEach(inp=>{
+      let arr = inp.split(';');
+      let word = arr[2].split(' ');
+    if(arr[0] == 'C') {
+            switch (arr[1]) {
+            case 'M':
+              let pw = word.map(w=>titleCase(w)).join('').trim();
+              console.log(pw.charAt(0).toLowerCase() + pw.slice(1) + '()');
+              break;
+            case 'V':
+              let pwV = word.map(w=>titleCase(w)).join('').trim();
+             console.log(pwV.charAt(0).toLowerCase() + pwV.slice(1));
+              break;
+            case 'C':
+             console.log(word.map(w=>titleCase(w)).join('').trim())
+              break;
+          }  
+    }
+    else if(arr[0] == 'S') {
+            switch (arr[1]) {
+            case 'M':
+             console.log(arr[2].split(/\W+|(?=[A-Z])|_/g).join(' ').toLowerCase());
+              break;
+            case 'V':
+              console.log(arr[2].split(/\W+|(?=[A-Z])|_/g).join(' ').toLowerCase());
+              break;
+            case 'C':
+               console.log(arr[2].match(/[A-Z][a-z]+|[0-9]+/g).map(w=>w.toLowerCase()).join(' '));
+              break;
+          }
+       }
+       });
+    }
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    _input = "";
+    process.stdin.on("data", function (input) {
+        _input += input;
+    });
+    
+    process.stdin.on("end", function () {
+       processData(_input);
+    });
